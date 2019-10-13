a235b12c65cc7a8601ca2e9c1f958861c028df1d
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:39:14 -0500

    Create PracticeM_Chapter1_SelfCheck10.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeM_Chapter1_SelfCheck10.java
@@ -0,0 +1,17 @@
+package book.practice.assignments;
+
+//// Name: Sarai Hannah Ajai
+//// Online Course: Learn to Program in Java (Microsoft Dev276x)
+//// Book: Building Java Programs 5e
+//// Chapter 1 - Self-Check Number 10
+//// Instructor: Kasey Champion
+//// Date: October 12, 2019
+
+public class PracticeM_Chapter1_SelfCheck10 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Shaw is 7'1");
+        System.out.println("The string \"\" is an empty message");
+        System.out.println("\\'\"\"");
+    }
+}
