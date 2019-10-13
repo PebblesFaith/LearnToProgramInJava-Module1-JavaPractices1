f646398cea4594fe08e8ac91af559add7bf16536
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 12:52:25 -0500

    Create PracticeW_Chapter1_ProgramErrors20.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Program Errors problem number 20 dated on October 13, 2019.

---
+++ PracticeW_Chapter1_ProgramErrors20.java
@@ -0,0 +1,30 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Program Errors Number 20
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+public class PracticeW_Chapter1_ProgramErrors20 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("The first error is in the beginning of the curly brace \"{\" was missing in the class header.");
+        System.out.println("And, the second error is in the \"args\" argument was missing from the method header.");
+        System.out.println("Four score and seven years ago,");
+        System.out.println("our fathers brought forth on");
+        System.out.println("this continent a new nation");
+        System.out.println("conceived in liberty");
+        System.out.println("and dedicated to the proposition");
+        System.out.println("that"); // this part should
+        System.out.println("The third error is in the above print line statement (20) comment shows an illegal asterisk and forward slash that should have been two forward slashes next to a print line statement to the right.");
+        System.out.println("all");  // really say,
+        System.out.println("The fourth error is in the above print line statement (22) does not show a forward slash that should have been two forward slashes next to a print line statement to the right.");
+        System.out.println("men"); // "all PEOPLE!"
+        System.out.println("The fifth error is in the above print line statement (24) does not show a forward slash that should have been two forward slashes next to a print line statement to the right.");
+        System.out.println("are");
+        System.out.println("created");
+        System.out.println("equal");
+    }
+}
