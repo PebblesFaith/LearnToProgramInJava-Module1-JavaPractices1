3fa4830dce66aed11a722327ab9f0a9b7bda3f76
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 17:38:38 -0500

    Create PracticeAG_Chapter1_ProceduralDecomposition31.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Procedural Decomposition problem number 31 dated on October 13, 2019.

---
+++ PracticeAG_Chapter1_ProceduralDecomposition31.java
@@ -0,0 +1,23 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Procedural Decomposition Number 31
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+// This program is written in Java language code in order to restructure procedural decomposition
+// number 31 that is too difficult to read, as a programmer.
+public class PracticeAG_Chapter1_ProceduralDecomposition31 {
+    public static void main(String[] args) {
+        System.out.println("Programs can be easy or");
+        System.out.println("difficult to read, depending");
+        System.out.println("upon their formant.");
+        System.out.println();
+        System.out.println("Everyone, including yourself,");
+        System.out.println("will be happier if you choose");
+        System.out.println("to format your programs,");
+        System.out.println("as you have learned in your Java language studies on format structures.");
+    }
+}
