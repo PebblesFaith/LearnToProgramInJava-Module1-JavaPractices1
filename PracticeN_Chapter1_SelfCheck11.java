97645e145570c163a10bf99633c2a21838ab47e6
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:39:28 -0500

    Create PracticeN_Chapter1_SelfCheck11.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeN_Chapter1_SelfCheck11.java
@@ -0,0 +1,19 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Self-Check Number 11
+// Instructor: Kasey Champion
+// Date: October 12, 2019
+
+public class PracticeN_Chapter1_SelfCheck11 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("\ta\tb\tc");
+        System.out.println("\\\\");
+        System.out.println("'");
+        System.out.println("\"\"\"");
+        System.out.println("C:\nin\the downward spiral");
+    }
+}
