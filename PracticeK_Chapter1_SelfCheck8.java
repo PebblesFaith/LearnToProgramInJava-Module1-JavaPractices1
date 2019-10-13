2afbc60a7e17e4cc19d8edf51998a93047556cfc
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:38:57 -0500

    Create PracticeK_Chapter1_SelfCheck8.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeK_Chapter1_SelfCheck8.java
@@ -0,0 +1,18 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Self-Check Number 8
+// Instructor: Kasey Champion
+// Date: October 12, 2019
+
+
+public class PracticeK_Chapter1_SelfCheck8 {
+    public static  void main(String[] agrs) {
+        System.out.println("\"Quotes\"");
+        System.out.println("Slashes \\//");
+        System.out.println("How '\"confounding' \"\\\" it is!");
+    }
+
+}
