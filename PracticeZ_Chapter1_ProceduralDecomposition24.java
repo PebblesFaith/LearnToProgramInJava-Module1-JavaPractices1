0e8a3d641787b1d6ab0985e81e56a6457c6870e0
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 13:53:01 -0500

    Create PracticeZ_Chapter1_ProceduralDecomposition24.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Procedural Decomposition problem number 24 dated on October 13, 2019.

---
+++ PracticeZ_Chapter1_ProceduralDecomposition24.java
@@ -0,0 +1,32 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Procedural Decomposition Number 24
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+public class PracticeZ_Chapter1_ProceduralDecomposition24 {
+    public static void first() {
+        System.out.println("Inside the first method");
+    }
+
+    public static void second() {
+        System.out.println("Inside the second method");
+        first();
+    }
+
+    public static void third() {
+        first();
+        second();
+        System.out.println("Inside the third method");
+    }
+
+    public static void main(String[] args) {
+        first();
+        third();
+        second();
+        third();
+    }
+}
