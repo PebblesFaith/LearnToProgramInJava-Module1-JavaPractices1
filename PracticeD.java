3aa3fb6d11eeecb34825fba3bb0a019885a56e4c
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:37:40 -0500

    Create PracticeD.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeD.java
@@ -0,0 +1,10 @@
+package book.practice.assignments;
+
+public class PracticeD {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println();
+        System.out.println("This program produces four");
+        System.out.println("lines of output in Java language.");
+    }
+}
