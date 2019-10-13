31540d6a091c05d443640494f12b357a6bdad1d4
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:39:49 -0500

    Create PracticeP_Chapter1_SelfCheck13.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeP_Chapter1_SelfCheck13.java
@@ -0,0 +1,17 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Self-Check Number 13
+// Instructor: Kasey Champion
+// Date: October 12, 2019
+
+public class PracticeP_Chapter1_SelfCheck13 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("\"Several slashes are sometimes seen,\"");
+        System.out.println("said Sally. \"I've said so.\" See?");
+        System.out.println("I am having difficulty with slash quotation within a quote I will need to restudy this section again.");
+    }
+}
