8bd1f214992b81229a0e33354f90077724760509
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:40:31 -0500

    Create PracticeU_Chapter1_ProgramErrors18.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeU_Chapter1_ProgramErrors18.java
@@ -0,0 +1,19 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Program Errors Number 18
+// Instructor: Kasey Champion
+// Date: October 12, 2019
+
+public class PracticeU_Chapter1_ProgramErrors18 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("First error is the word class was missing from the class header.");
+        System.out.println("This is a test of the");
+        System.out.println("The second error in the \'printIn statement\' was missing a semicolon in the above print line statement.");
+        System.out.println("emergency broadcast system.");
+        System.out.println("And, the third error was the \'PrintIn statement\' in the letter \'P\' should have been capitalized causing a syntax error in the above print line statement.");
+    }
+}
