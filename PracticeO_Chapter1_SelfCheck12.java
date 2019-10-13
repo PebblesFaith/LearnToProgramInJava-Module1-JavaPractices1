02dc4c2eb0feae30f9cb454a10b91299ef934b9a
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:39:42 -0500

    Create PracticeO_Chapter1_SelfCheck12.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeO_Chapter1_SelfCheck12.java
@@ -0,0 +1,20 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Self-Check Number 12
+// Instructor: Kasey Champion
+// Date: October 12, 2019
+
+public class PracticeO_Chapter1_SelfCheck12 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Dear \"DoubleSlash\" magazine");
+        System.out.println();
+        System.out.println("\tYour publication confuses me. Is it");
+        System.out.println("a \\\\ slash or a //// slash?");
+        System.out.println("\nSincerely,");
+        System.out.println("Susan \"Suzy\" Smith");
+    }
+}
