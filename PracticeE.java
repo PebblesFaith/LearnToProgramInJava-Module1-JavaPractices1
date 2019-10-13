818b40321a8b38575fa4b7001868ecfdd0791e10
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:37:50 -0500

    Create PracticeE.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeE.java
@@ -0,0 +1,12 @@
+package book.practice.assignments;
+
+public class PracticeE {
+    public static void main(String[]  args) {
+        System.out.print("To be ");
+        System.out.print("or not to be. ");
+        System.out.print("That is ");
+        System.out.print("the question. ");
+        System.out.print("This is");
+        System.out.print(" for the whole family!");
+    }
+}
