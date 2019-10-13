dea0d3cb2265fbacfed9c95832284e6cd9949655
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:40:21 -0500

    Create PracticeT_Chapter1_SelfCheck17.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeT_Chapter1_SelfCheck17.java
@@ -0,0 +1,24 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Self-Check Number 17
+// Instructor: Kasey Champion
+// Date: October 12, 2019
+
+public class PracticeT_Chapter1_SelfCheck17 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("some lines of code");
+        System.out.println("have // characters on them");
+        System.out.println("which means ");  //that they are comments
+        System.out.println("written by the programmer.");
+        System.out.println();
+        System.out.println("lines can also");
+        System.out.println("have /* and */ characters");
+        /* System.out.printIn("which represents");
+        System.out.printIn("a multi-line style");
+        */ System.out.println("of comments");
+    }
+}
