498e10b8ef87ccc1e9fcfe7b251dead7c3cfa26a
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 12:51:36 -0500

    Create PracticeV_Chapter1_ProgramErrors19.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Program Errors problem number 19 dated on October 12, 2019.

---
+++ PracticeV_Chapter1_ProgramErrors19.java
@@ -0,0 +1,20 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Program Errors Number 19
+// Instructor: Kasey Champion
+// Date: October 12, 2019
+
+public class PracticeV_Chapter1_ProgramErrors19 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("The first error is the \'void\' was missing from the method header.");
+        System.out.println("And, the second error is the \'string\' first letter should have been capitalized inside the parentheses of the method header.");
+        System.out.println("Speak friend");
+        System.out.println("and enter");
+        System.out.println("The third error is the ending quotation mark was missing for the second print line statement.");
+        System.out.println("And, the fourth error is the ending curly brace \"}\" is missing for the method header.");
+    }
+}
