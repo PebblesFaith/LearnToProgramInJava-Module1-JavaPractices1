bd2383a355c42016be60c700d7559404d9e17312
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 13:18:27 -0500

    Create PracticeX_Chapter1_ProceduralDecomposition22.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Procedual Decomposition problem number 22 dated on October 13, 2019.

---
+++ PracticeX_Chapter1_ProceduralDecomposition22.java
@@ -0,0 +1,24 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Procedural Decomposition Number 22
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+public class PracticeX_Chapter1_ProceduralDecomposition22 {
+    public static void main(String[] args) {
+        message1();
+        message2();
+        System.out.println("Done with main.");
+    }
+    public static void message1() {
+        System.out.println("This is message 1.");
+    }
+
+    public static void message2() {
+        System.out.println("This is message 2.");
+        message1();
+        System.out.println("Done with message 2.");
+    }
+}
