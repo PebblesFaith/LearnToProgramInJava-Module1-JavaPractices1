e1098e7a24d4f8e38a590313e64567e6f94a7613
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:39:56 -0500

    Create PracticeQ_Chapter1_SelfCheck14.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeQ_Chapter1_SelfCheck14.java
@@ -0,0 +1,22 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Self-Check Number 14
+// Instructor: Kasey Champion
+// Date: October 12, 2019
+
+public class PracticeQ_Chapter1_SelfCheck14 {
+    public static void main (String[] args) {
+        System.out.println();
+        System.out.println("This is a test of your");
+        System.out.println("knowledge of \"quotes\" used");
+        System.out.println("in \'string literals.\'");
+        System.out.println();
+        System.out.println("You\'re to \"get it right\"");
+        System.out.println("if you read the section on");
+        System.out.println("\''quotes.\''");
+
+    }
+}
