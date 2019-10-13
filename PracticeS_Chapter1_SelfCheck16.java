591d7fe9eac40621942e27eeadf426d6e6915a8e
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:40:14 -0500

    Create PracticeS_Chapter1_SelfCheck16.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeS_Chapter1_SelfCheck16.java
@@ -0,0 +1,24 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Self-Check Number 16
+// Instructor: Kasey Champion
+// Date: October 12, 2019
+
+public class PracticeS_Chapter1_SelfCheck16 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Twas ");
+        System.out.println("brillig and the");
+        System.out.println(" ");
+        System.out.println(" slithy toves did");
+        System.out.println(" ");
+        System.out.println("gyre and");
+        System.out.println("gimble");
+        System.out.println();
+        System.out.println( "in the wabe.");
+    }
+}
+
