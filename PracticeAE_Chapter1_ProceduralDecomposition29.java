22b30e91225d1c2f06ef121cc9c63303caeaf081
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 16:11:51 -0500

    Create PracticeAE_Chapter1_ProceduralDecomposition29.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Procedural Decomposition problem number 29 dated on October 13, 2019.

---
+++ PracticeAE_Chapter1_ProceduralDecomposition29.java
@@ -0,0 +1,26 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Procedural Decomposition Number 29
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+public class PracticeAE_Chapter1_ProceduralDecomposition29 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+        message();
+    }
+
+    public static void message() {
+        System.out.println("This program surely cannot ");
+        System.out.println("have any \"error\" in it");
+        System.out.println("I have found the first error in the class header in the \"Errors\" word does not belong in the class header.");
+        System.out.println("And, I have found the second and third errors are in the method header in the method identifier name \'main\' parameter \"String\" is missing the open and close bracket.");
+        System.out.println("The fourth and fifth errors are located within the method header statement, \"message()\" is missing the semicolon \';\' at the end of statement to call the method.");
+        System.out.println("And, the sixth and seventh errors are located in the second method header to define the call method is missing the open and close parentheses \"()\".");
+        System.out.println("I have found the eighth and ninth errors are located in print line statement line number (18), the open and close quotation mark \'\\' was missing from the print statement line quote. ");
+        System.out.println("The last, number tenth error is shown in the closing curly braces \"}\" in the class method statement.");
+    }
+}
