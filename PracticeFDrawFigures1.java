b35ea72eb01f553f1da7665589a84aeb84a78e07
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:38:00 -0500

    Create PracticeFDrawFigures1.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeFDrawFigures1.java
@@ -0,0 +1,38 @@
+package book.practice.assignments;
+
+public class PracticeFDrawFigures1 {
+    public static void main(String[] args) {
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /     \\");
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+        System.out.println();
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /     \\");
+        System.out.println();
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /     \\");
+        System.out.println("+-------+");
+        System.out.println("|       |");
+        System.out.println("|       |");
+        System.out.println("+-------+");
+        System.out.println("|United |");
+        System.out.println("|States |");
+        System.out.println("+-------+");
+        System.out.println("|       |");
+        System.out.println("|       |");
+        System.out.println("+-------+");
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /     \\");
+        System.out.println();
+        System.out.println("Sarai Hannah Ajai's DrawFigures1 class name Exercise Dated October 10, 2019");
+    }
+}
