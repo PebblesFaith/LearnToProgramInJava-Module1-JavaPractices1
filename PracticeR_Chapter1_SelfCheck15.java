712afafa774851ad6d77cf06087fa25b8092ae3b
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 10:40:05 -0500

    Create PracticeR_Chapter1_SelfCheck15.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, self-check problems, and Now Java, and Program Errors dated on October 12, 2019.

---
+++ PracticeR_Chapter1_SelfCheck15.java
@@ -0,0 +1,15 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Self-Check Number 15
+// Instructor: Kasey Champion
+// Date: October 12, 2019
+
+public class PracticeR_Chapter1_SelfCheck15 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("\\ / \\\\ // \\\\\\ ///");
+    }
+}
