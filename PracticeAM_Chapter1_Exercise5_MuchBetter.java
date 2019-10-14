1847a2f86150acebfc0227bbd43d6e4a2f507a13
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 22:04:50 -0500

    Create PracticeAM_Chapter1_Exercise5_MuchBetter.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Exercise problem number 5 dated on October 13, 2019.

---
+++ PracticeAM_Chapter1_Exercise5_MuchBetter.java
@@ -0,0 +1,21 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Exercise Number 5
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+// Write a complete Java program called MuchBetter that prints the following output:
+public class PracticeAM_Chapter1_Exercise5_MuchBetter {
+    public static void main(String[] args) {
+        System.out.println("A \"quoted\" String is");
+        System.out.println("\'much\' better if you learn");
+        System.out.println("the rules of \"escape sequences.\"");
+        System.out.println("Also, a double quotation marks \"\" represents an empty String.");
+        System.out.println("Don't forget: use the escape sequences character \\\" instead of \"");
+        System.out.println("or \'\' is not the same as a single quotation mark \".");
+    }
+
+}
