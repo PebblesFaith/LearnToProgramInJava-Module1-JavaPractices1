d96ea1850453cf9bbba1e07862dbd446bd1b406c
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 19:45:38 -0500

    Create PracticeAH_Chapter1_ProceduralDecomposition32.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Procedural Decomposition problem number 32 dated on October 13, 2019.

---
+++ PracticeAH_Chapter1_ProceduralDecomposition32.java
@@ -0,0 +1,21 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Procedural Decomposition Number 32
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+// This program is written in Java language code in order to restructure procedural decomposition format
+// number 32 that is too difficult to read, as a programmer.
+
+public class PracticeAH_Chapter1_ProceduralDecomposition32 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("I really wish that");
+        System.out.println("I had formatted, by source");
+        System.out.println("code correctly!");
+    }
+
+}
