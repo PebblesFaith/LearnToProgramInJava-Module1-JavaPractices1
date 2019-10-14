dc99463c595b2fe6beebe4c5fb369d3ee7a7b786
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 22:40:06 -0500

    Create PracticeAN_Chapter1_Exercise6_Meta.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Exercise problem number 6 dated on October 13, 2019.

---
+++ PracticeAN_Chapter1_Exercise6_Meta.java
@@ -0,0 +1,24 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Exercise Number 6
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+// Write a complete Java program call Meta whose output is the text that would be the source code
+// of a Java program that prints “Hello, World!, as it output.
+public class PracticeAN_Chapter1_Exercise6_Meta {
+    public static void main(String[] args) {
+        System.out.println("Step 1: Use editor to create/edit your source file.");
+        System.out.println("In some IDEs, Step 1 and Step 2 are combined");
+        System.out.println("in these environments the process of compiling is more incremental");
+        System.out.println("when you submit source file to the Java's compiler.");
+        System.out.println("If you receive a compiling error then your returns to step 1 and,");
+        System.out.println("If you program compiled successfully your source file will print");
+        System.out.println("your output onto the console such as,");
+        System.out.println("Hello, World!");
+        System.out.println("And, step 3: IDEs execute your Java's class file.");
+    }
+}
