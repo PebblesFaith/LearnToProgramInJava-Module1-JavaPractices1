8fcf914f4e2b9502517af43692642fae7d5d016b
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 21:43:30 -0500

    Create PracticeAL_Chapter1_Exercise4_Difference.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Exercise problem number 4 dated on October 13, 2019.

---
+++ PracticeAL_Chapter1_Exercise4_Difference.java
@@ -0,0 +1,18 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Exercise Number 4
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+// Write a complete Java program called Difference that prints the following output:
+public class PracticeAL_Chapter1_Exercise4_Difference {
+    public static void main(String[] args) {
+        System.out.println("What is the difference between");
+        System.out.println("a \' and a \"? Or between a \" and a \\\"?");
+        System.out.println("One is what, we see when we are typing our program");
+        System.out.println("The other is what appears on the \"console\".");
+    }
+}
