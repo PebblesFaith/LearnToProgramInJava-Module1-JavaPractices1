9b5ed60668e64613cc78e4ed5406cc332adcb4ce
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 20:33:45 -0500

    Create PracticeAI_Chapter1_Exercise1_Stewie.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Exercise problem number1 dated on October 13, 2019.

---
+++ PracticeAI_Chapter1_Exercise1_Stewie.java
@@ -0,0 +1,22 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Exercise Number 1
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+//
+// What a complete Java program called Stewie that print the following output:
+public class PracticeAI_Chapter1_Exercise1_Stewie {
+    public static void main(String[] args) {
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.print("\\\\\\\\\\");
+        System.out.print("\\\\\\\\\\");
+        System.out.print("\\\\\\\\\\");
+        System.out.print("\\\\\\\\\\");
+        System.out.print("\\\\");
+    }
+}
