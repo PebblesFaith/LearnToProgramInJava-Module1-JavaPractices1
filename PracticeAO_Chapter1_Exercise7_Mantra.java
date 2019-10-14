7e9c80960edce2b7d69dce0314f03a77bb7703a6
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 22:58:11 -0500

    Create PracticeAO_Chapter1_Exercise7_Mantra.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Exercise problem number 7 dated on October 13, 2019.

---
+++ PracticeAO_Chapter1_Exercise7_Mantra.java
@@ -0,0 +1,21 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Exercise Number 7
+// Date: October 13, 2019
+
+// Write a complete Java program called Mantra that prints that prints
+// the following output. Use at least one static method besides main.
+public class PracticeAO_Chapter1_Exercise7_Mantra {
+    public static void main(String[] args) {
+        command1();
+        command1();
+    }
+
+    public static void command1() {
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.");
+    }
+}
