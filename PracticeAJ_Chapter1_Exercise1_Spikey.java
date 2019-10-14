5d3cc507bbffd03264faed742530d8a580fc4952
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 20:55:05 -0500

    Create PracticeAJ_Chapter1_Exercise1_Spikey.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Exercise problem number 2 dated on October 13, 2019.

---
+++ PracticeAJ_Chapter1_Exercise1_Spikey.java
@@ -0,0 +1,21 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Exercise Number 2
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+// 2.	Write a complete Java program called Spikey that print the following output:
+public class PracticeAJ_Chapter1_Exercise1_Spikey {
+    public static void main(String[] args) {
+        System.out.println("   \\/");
+        System.out.println("  \\\\//");
+        System.out.println(" \\\\\\///");
+        System.out.println(" ///\\\\\\");
+        System.out.println("  //\\\\");
+        System.out.println("   /\\");
+    }
+
+}
