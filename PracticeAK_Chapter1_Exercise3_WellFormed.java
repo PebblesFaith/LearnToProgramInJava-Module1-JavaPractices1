8c8f2a74eca2e87d854df70a9c3ff098e3bf01fc
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-13 21:16:09 -0500

    Create PracticeAK_Chapter1_Exercise3_WellFormed.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Exercise problem number 3 dated on October 13, 2019.

---
+++ PracticeAK_Chapter1_Exercise3_WellFormed.java
@@ -0,0 +1,23 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Exercise Number 3
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+// 3.	Write a complete Java program called WellFormed that prints the following output:
+public class PracticeAK_Chapter1_Exercise3_WellFormed {
+    public static void main(String[] args) {
+        System.out.println("A well-formed Java program has");
+        System.out.println("a main method with { and }");
+        System.out.println("braces.");
+        System.out.println();
+        System.out.println("A system.out.println statement");
+        System.out.println("has { and } braces and usually a");
+        System.out.println("String that starts and ends");
+        System.out.println("with a \" quotation character.");
+        System.out.println("(But we type \"\\ instead!)");
+    }
+}
