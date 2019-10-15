18eab19ef97bb486bf244ae0e975c1d2a45f9cf9
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-14 19:42:43 -0500

    Create PracticeAR_Chapter1_Exercise10_Egg2.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Exercise problem number 10 dated on October 14, 2019.

---
+++ PracticeAR_Chapter1_Exercise10_Egg2.java
@@ -0,0 +1,73 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Exercise Number 10
+// Instructor: Kasey Champion
+// Date: October 14, 2019
+
+// Modify the program from the previous exercise to become a new Egg2 that displays the following output.
+// Use static methods as appropriate.
+public class PracticeAR_Chapter1_Exercise10_Egg2 {
+    public static void main(String[] agrs) {
+    line1();
+    slashes1();
+    slashes2();
+    slashes3();
+    slashes4();
+    line2();
+    System.out.println();
+    System.out.println();
+    middleDesign();
+    System.out.println();
+    line1();
+    slashes1();
+    slashes2();
+    slashes3();
+    slashes4();
+    line2();
+    System.out.println();
+    System.out.println();
+    middleDesign();
+    slashes3();
+    slashes4();
+    line2();
+    System.out.println();
+    System.out.println();
+    line1();
+    slashes1();
+    slashes2();
+    middleDesign();
+    slashes3();
+    slashes4();
+    line2();
+
+}
+
+    public static void line1() {
+        System.out.println("     _________        ");
+    }
+    public static void slashes1() {
+        System.out.println("    /         \\        ");
+    }
+    public static void slashes2() {
+        System.out.println("   /           \\        ");
+    }
+    public static void middleDesign() {
+        System.out.println("   -\"-\"-\"-\"-\"-\"-  ");
+    }
+
+    public static void slashes3() {
+        System.out.println("   \\           /        ");
+    }
+    public static void slashes4() {
+        System.out.println("    \\         /        ");
+    }
+    public static void line2() {
+        System.out.print("     ________       ");
+    }
+
+}
+
+
