b18c278022e4a1747c9a04ef7c5a00fc99ec1293
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-10-14 19:18:20 -0500

    Create PracticeAQ_Chapter1_Exercise9_Egg.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 1, Exercise problem number 9 dated on October 14, 2019.

---
+++ PracticeAQ_Chapter1_Exercise9_Egg.java
@@ -0,0 +1,46 @@
+package book.practice.assignments;
+
+// Name: Sarai Hannah Ajai
+// Online Course: Learn to Program in Java (Microsoft Dev276x)
+// Book: Building Java Programs 5e
+// Chapter 1 - Exercise Number 9
+// Instructor: Kasey Champion
+// Date: October 13, 2019
+
+// Write a program called Egg that displays the following output:
+public class PracticeAQ_Chapter1_Exercise9_Egg {
+    public static void main(String[] args) {
+        line1();
+        slashes1();
+        slashes2();
+        middleDesign();
+        slashes3();
+        slashes4();
+        line2();
+
+    }
+
+    public static void line1() {
+        System.out.println("     _________        ");
+    }
+    public static void slashes1() {
+        System.out.println("    /         \\        ");
+    }
+    public static void slashes2() {
+        System.out.println("   /           \\        ");
+    }
+    public static void middleDesign() {
+        System.out.println("   -\"-\"-\"-\"-\"-\"-  ");
+    }
+
+    public static void slashes3() {
+        System.out.println("   \\           /        ");
+    }
+    public static void slashes4() {
+        System.out.println("    \\         /        ");
+    }
+    public static void line2() {
+        System.out.print("     ________       ");
+    }
+
+}
